581cdb2beaca333d24a11c5caa1acd2655d4f3eb
ChoEun-Sang <wh94es@gmail.com>
2023-07-07 14:49:46 +0900

    Feat(fe): App.js 캘린터 컴포넌트 출력 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import TheCalendar from './components/calendar/TheCalendar';
 function App() {
   return (
     <div className="App">
       <h2>hello2</h2>
+      <TheCalendar />
     </div>
   );
 }
