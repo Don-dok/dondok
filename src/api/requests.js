498c29a2923a67468f21b473608c9d3e0ff2cdf3
ChoEun-Sang <wh94es@gmail.com>
2023-07-11 13:44:32 +0900

    Feat(fe): 소비 기록 작성 및 조회 API 연결

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import axios from 'axios'
 const BASE_URL = " http://52.78.195.183:3003/api/expenses"
 
@@ -18,24 +18,22 @@ const postSpending = async () => {
   }
 }
 // 일별, 주별, 월별 소비 조회
-const lookupByDate = async (period, userID) => {
-  try {
-    const res = await axios.get(`${BASE_URL}/summary?period=${period}&userID=${userID}`);
-    return console.log('날짜별 소비 조회 성공',res.data);
-  } catch (error) {
-    return console.log('날짜별 소비 조회 실패',error);
-  }
+const lookupByDate = async (period) => {
+    const res = await axios.get(`${BASE_URL}/summary?period=${period}&userId=Team2`);
+    console.log('일주월별 상태', res.status);
+    if (res.status === 200) {
+      console.log('일별, 주별, 월별 data', res.data);
+      return res.data
+    } else return false;
 }
 
 // 소비 기록 달력 호출 
 const getSpendingCalendar = async (year, month, userId) => {
-  try {
     const res = await axios.get(`${BASE_URL}/calendar?year=${year}&month=${month}&userId=${userId}`);
-    return console.log('소비달력성공',res.data);
-  } catch (error) {
-    return console.log('소비달력실패',error);
-  }
-
+    if (res.status === 200) {
+      console.log('소비 기록 달력 data', res.data);
+      return res.data
+    } else return false;
 }
 
 export {postSpending, lookupByDate, getSpendingCalendar}
