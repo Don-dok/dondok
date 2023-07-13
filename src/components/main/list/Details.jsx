6577a073fd9d1fd9336a008c3f6cd6cfccd88cfd
ChoEun-Sang <wh94es@gmail.com>
2023-07-13 11:23:14 +0900

    Style(fe): Details 컴포넌트 주석 추가

--- src/components/main/list/Details.jsx
+++ src/components/main/list/Details.jsx
@@ -27,7 +27,7 @@ const Details = ({dateData, details, isDaily}) => {
     }
   }
 
-  // 리스트 상세 내역에 출력할 일별 지출 내역
+  // item 속성에서 출력할 일별 지출 데이터 가공
   const getListData = () => {
     let spendingList = [];
     // spending 객체 for in 반복문으로 각 key: value 접근
@@ -42,27 +42,30 @@ const Details = ({dateData, details, isDaily}) => {
     return spendingList;
   };
 
-// item 속성 - daily일 때
+// 컴포넌트의 item 속성에 들어갈 값 - daily일 때
+// items는 items =[{}] 형태로 들어가야 함
 const dailyItems = () => {
 const data = getListData();
 console.log(data);
-
-return data.sort((a,b)=> new Date(b.date) - new Date(a.date)).map((value, i)=> {return {
-  key: i,
-  label: (
-    <Container>
-      <p style={{fontWeight: 700}}>{value.date}</p>
-      <p style={{color: '#eb2f96'}}>₩ {formatPrice(value.sum)}</p>
-    </Container>
-  ),
-  children: (
-    value.data.sort((a,b)=> new Date(b.date) - new Date(a._date)).map((value, i)=>(
-      <Container key={i}>
-        <li>
-          <p>{value.category}</p>
-          <p>₩{formatPrice(value.amount)}</p>
-        </li>
+// 데이터를 최근 날짜 기준으로 sort
+return data.sort((a,b)=> new Date(b.date) - new Date(a.date)).map((value, i)=> {
+  return {
+    key: i,
+    label: (
+      <Container>
+        <p style={{fontWeight: 700}}>{value.date}</p>
+        <p style={{color: '#eb2f96'}}>₩ {formatPrice(value.sum)}</p>
       </Container>
+    ),
+    // 상세 내역
+    children: (
+      value.data.sort((a,b)=> new Date(b.date) - new Date(a._date)).map((value, i)=>(
+        <Container key={i}>
+          <li>
+            <p>{value.category}</p>
+            <p>₩{formatPrice(value.amount)}</p>
+          </li>
+        </Container>
     ))
   )
 
@@ -70,7 +73,7 @@ return data.sort((a,b)=> new Date(b.date) - new Date(a.date)).map((value, i)=> {
 
 }
 
-// item 속성 - daily 제외
+// 컴포넌트의 item 속성에 들어갈 값 - daily 제외
   // eslint-disable-next-line react/prop-types
   const items = dateData.sort((a,b)=> new Date(b._id) - new Date(a._id)).map((value, i)=> {
     return {
@@ -105,6 +108,7 @@ return data.sort((a,b)=> new Date(b.date) - new Date(a.date)).map((value, i)=> {
         {
           <Collapse 
             accordion 
+            // daily 일때 사용하는 API가 다르기 때문에 구분
             items={isDaily? dailyItems() : items }
             onChange={(value)=> setSelectedDate(value[0]?.split('-').map(value=> Number(value)))}
             />
