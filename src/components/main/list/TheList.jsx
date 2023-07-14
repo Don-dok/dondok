37b10f8dd1c20a0031f3ca783034eb7487f3b592
ChoEun-Sang <wh94es@gmail.com>
2023-07-14 17:27:56 +0900

    Style(fe): 리스트 컴포넌트 변경으로 인해 사소한 코드 변경

--- src/components/main/list/TheList.jsx
+++ src/components/main/list/TheList.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import { lookupByDate } from '../../../api/requests'
+import React, { useEffect, useState } from 'react';
+import { lookupByDate } from '../../../api/requests';
 import { Segmented, Space } from 'antd';
 import Details from './Details';
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const TheList = () => {
   const [options, setOptions] = useState(['Daily', 'Weekly', 'Monthly']);
@@ -13,49 +13,44 @@ const TheList = () => {
   const [weekly, setWeekly] = useState([]);
   const [monthly, setMonthly] = useState([]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     getSelectedDate();
-  },[selectedDate])
-
+  }, [selectedDate]);
 
   // selectedDate에 따라 데이터 호출
   const getSelectedDate = () => {
-    lookupByDate('daily').then(data => setDaily(data));
-    lookupByDate('weekly').then(data => setWeekly(data));
-    lookupByDate('monthly').then(data => setMonthly(data));
+    lookupByDate('daily').then((data) => setDaily(data));
+    lookupByDate('weekly').then((data) => setWeekly(data))
+    lookupByDate('monthly').then((data) => setMonthly(data));
   };
 
-// selectedDate에 따라 props 변경되는 컴포넌트
+  // selectedDate에 따라 props 변경되는 컴포넌트
   const TheDetails = () => {
     if (selectedDate === 'daily') {
-      return <Details dateData={daily} details={daily} isDaily={isDaily}/>
+      return <Details dateData={daily} details={daily} isDaily={isDaily} />;
     } else if (selectedDate === 'weekly') {
-      return <Details dateData={weekly} details={daily} isWeekly={isWeekly}/>
+      return <Details dateData={weekly} details={daily} isWeekly={isWeekly} />;
     } else {
-      return <Details dateData={monthly} details={weekly}/>
+      return <Details dateData={monthly} details={weekly} />;
     }
-  }
+  };
   return (
     <Container>
-    <Space direction="vertical">
-      <Segmented 
-        options={options} 
-        onChange={(value)=> setSelectedDate(value.toLowerCase())}
+      <Space direction="vertical">
+        <Segmented
+          options={options}
+          onChange={(value) => setSelectedDate(value.toLowerCase())}
         />
-    </Space>
-    <TheDetails />
+      </Space>
+      <TheDetails />
     </Container>
-
-
   );
 };
 
-
-export default TheList
+export default TheList;
 
 const Container = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
