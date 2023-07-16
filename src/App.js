32e009c16a570c43bcfb00e61636742a0ed17a2c
ChoEun-Sang <wh94es@gmail.com>
2023-07-16 10:57:23 +0900

    Feat(fe): 하단 탭 children 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TheTabs from './components/common/TheTabs';
 import TheHeader from './components/common/TheHeader';
 import './App.css';
 import { styled } from 'styled-components';
+import SubTabs from './components/main/SubTabs';
+import Search from 'antd/es/transfer/search';
+import ChartWrpper from './components/stastics/ChartWrapper';
+import User from '../src/components/user/User';
+
 function App() {
+  const [activeKey, setActiveKey] = useState('');
+
+  const Components = () => {
+    if (activeKey === '1' || activeKey === '') {
+      return <SubTabs />;
+    } else if (activeKey === '2') {
+      return <Search />;
+    } else if (activeKey === '3') {
+      return <ChartWrpper />;
+    } else if (activeKey === '4') {
+      return <User />;
+    }
+  };
+
   return (
     <div className="App">
       <Container>
         <TheHeader />
-        <TheTabs />
+        <Components />
+        <TheTabs activeKey={activeKey} setActiveKey={setActiveKey} />
       </Container>
     </div>
   );
