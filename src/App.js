60357c6445ff8be783103e8063387545a758647f
LeeYongSoo <sideoff0217@naver.com>
2023-07-17 19:34:38 +0900

    Style(fe): User탭 디자인 변경

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import TheTabs from './components/common/TheTabs';
 import TheHeader from './components/common/TheHeader';
 import './App.css';
 import { styled } from 'styled-components';
+import 'reset-css';
+
+
 function App() {
   return (
     <div className="App">
       <Container>
-        <TheHeader />
+        <HeaderWrap>
+          <TheHeader />
+        </HeaderWrap>
         <TheTabs />
       </Container>
     </div>
@@ -18,11 +23,16 @@ export default App;
 
 const Container = styled.section`
   position: relative;
-  border: 1px solid #191919;
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 400px;
+  margin-top: 50px;
+`;
+
+const HeaderWrap = styled.div `
+  position: relative;
+  width:100%;
+
 `;
