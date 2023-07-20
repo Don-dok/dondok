e353b2341a7a3ec91c7221d34cc69f2412b5bd8c
ChoEun-Sang <wh94es@gmail.com>
2023-07-20 20:50:09 +0900

    Style(fe): 코드 정리

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
-import { Card, Avatar, Menu } from 'antd';
-import { GithubOutlined, RightOutlined } from '@ant-design/icons'
-import styled from 'styled-components'
-
+import { Card, Avatar, Menu, theme } from 'antd';
+import { GithubOutlined, RightOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 
 const { Meta } = Card;
 
-function getItem(
-  label,
-  key,
-  icon,
-  children,
-  type
-){
+function getItem(label, key, icon, children, type) {
   return {
     key,
     icon,
     children,
     label,
     type,
-  }
+  };
 }
 
+const { token } = theme;
 
 const items = [
-  getItem('공지사항', 'sub1',<RightOutlined />),
+  getItem('공지사항', 'sub1', <RightOutlined />),
   getItem('개인정보관리', 'sub2', <RightOutlined />),
-  getItem('FAQ', 'sub3',<RightOutlined />),
+  getItem('FAQ', 'sub3', <RightOutlined />),
   getItem('이용약관', 'sub4', <RightOutlined />),
-  getItem('계정설정', 'sub5',<RightOutlined />),
+  getItem('계정설정', 'sub5', <RightOutlined />),
 ];
-
 
 const StyledMenu = styled(Menu)`
   width: 400px;
@@ -40,14 +33,13 @@ const StyledMenu = styled(Menu)`
   margin-top: 10px;
 `;
 
-
 const User = () => {
   return (
     <Container>
-            <Card
+      <Card
         hoverable
         style={{ width: 400 }}
-        onClick={()=> window.open("https://github.com/Don-dok/dondok")}
+        onClick={() => window.open('https://github.com/Don-dok/dondok')}
         cover={
           <img
             alt="example"
@@ -56,17 +48,20 @@ const User = () => {
         }
       >
         <StyledMeta
-        avatar={<Avatar src="https://ih1.redbubble.net/image.3632891702.9945/st,small,845x845-pad,1000x1000,f8f8f8.jpg" />}
-        title="Team2"/>
-          <p>조은상, 주하림, 안태욱, 이용수</p>
-          <TextBox>
-            <GithubOutlined />
-            <p>https://github.com/Don-dok/dondok</p>
-          </TextBox>
+          theme={{ token: { colorBgBase: '#191919' } }}
+          avatar={
+            <Avatar src="https://ih1.redbubble.net/image.3632891702.9945/st,small,845x845-pad,1000x1000,f8f8f8.jpg" />
+          }
+          title="Team2"
+        />
+        <p>조은상, 주하림, 안태욱, 이용수</p>
+        <TextBox>
+          <GithubOutlined />
+          <p>https://github.com/Don-dok/dondok</p>
+        </TextBox>
       </Card>
       <StyledMenu mode="vertical" items={items} />
     </Container>
-
   );
 };
 
@@ -76,16 +71,17 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 20px;
-
-
-
-`
+`;
 const StyledMeta = styled(Meta)`
   display: flex;
   align-items: center;
-`
+
+  ul {
+    background-color: transparent;
+  }
+`;
 const TextBox = styled.div`
- color: #495057;
+  color: #495057;
   display: flex;
   align-items: center;
   gap: 10px;
@@ -93,7 +89,4 @@ const TextBox = styled.div`
     color: #868e96;
     margin: 0;
   }
-
-
-
-`
+`;
